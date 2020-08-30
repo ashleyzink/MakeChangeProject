775d57b0fa15027b1ab514bcb19af9de58c4b090
Ashley Zink <pamperedzink@gmail.com>
2020-08-29 20:24:26 -0600

    figured out trickle down econ. in method 3

--- src/makechange/MakeChange.java
+++ src/makechange/MakeChange.java
@@ -10,8 +10,10 @@ public class MakeChange {
 	public static void main(String[] args) {
 //		Hint: Mod operator
 //		shoppingAllDay();
-		double itemCost = shoppingAllDay();
-		costsTooMuch(itemCost);
+		double itemCostMain = shoppingAllDay();
+//		costsTooMuch(itemCost);
+		double amountOwedMain = costsTooMuch(itemCostMain);
+		makeChange(amountOwedMain);
 		
 	}
 
@@ -23,42 +25,54 @@ public class MakeChange {
 			double itemCost = input.nextDouble();
 			System.out.println("Cash machine only. No Debit. Please enter $" + itemCost);
 //			getItemCost.close();
-			return itemCost;
+			return itemCost; //1.33454
 			
 		}
 		
 //		User Story #2
 //		The user is then prompted asking how much money was tendered by the customer.
-//		User Story #3
-//		Display an appropriate message if the customer provided too little money or the exact amount.
 		private static double costsTooMuch(double itemCost) {
 			System.out.println("Change first, then bills. Please enter how much money was inserted: ");
 //			Scanner getAmountTendered = new Scanner(System.in); //Same issue as in shoppingAllDay() 
 			double amountTendered = input.nextDouble();
 			double amountOwed = amountTendered - itemCost;
 //			System.out.println(amountOwed + " : " + amountTendered + " : " + itemCost);
-			System.out.printf("Thank you! Your change will be: $ %.2f", amountOwed);
-			if (amountOwed > amountTendered) {
-				System.out.println("You're not quite there, put in more money.");
-			}
-			if (amountOwed == amountTendered) {
-				System.out.println("Thank you for put the exact amount!");
-			}
 //			getAmountTendered.close();
 			return amountOwed;
-			
-			
+		}
 
 			
-		}
-		
-//
+//		User Story #3
+//		Display an appropriate message if the customer provided too little money or the exact amount.
 //		User Story #4
 //		If the amount tendered is more than the cost of the item, 
 //		display the number of bills and coins that should be given to the customer.
-		
+			private static void makeChange(double amountOwed) {
+				int tens, fives, ones, quarters, dimes, nickles, pennies;
+				if (amountOwed < 0) {
+					System.out.println("You're not quite there, please deposit more money.");
+				}
+				else if (amountOwed == 0) {
+					System.out.println("Thank you for the exact amount during COVID-19. You're helping with the coin shortage.");
+				}
+				else {
+					System.out.printf("Thank you! Your change will be: $ %.2f\n", amountOwed);
+					tens = (int) amountOwed / 10;
+					if (tens == 1) {
+						System.out.print(tens + " ten dollar bill, ");
+					} 
+					amountOwed %= 10; 
+					fives = (int) amountOwed / 5;
+					if (fives == 1) {
+						System.out.print(fives + " five dollar bill, ");
+					}
+					
+				}
+				
+			}
 
-		// TODO Auto-generated method stub
+			
+		
 		
 
 
